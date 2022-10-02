9d8c11a6d34f3aead2aa9ff31c15eda7844e0e4e
Your Name <you@example.com>
2022-10-02 16:38:00 +0200

    Added next two days weather information

--- src/componets/Home/Home.js
+++ src/componets/Home/Home.js
@@ -17,26 +17,37 @@ const Home = ({loadWeatherRequest, weatherRedux,forecastdayList}) => {
             <SearchBar/>
             {weatherRedux.request?.error ? <NotFound/> 
             :
-            <div className={styles.infoWrapper}>
-                <div className={styles.dataWrapper}>
-                    <p className={styles.dayTitle}>{formatDate(weatherRedux.current?.last_updated)}</p>
-                    <p className={styles.cityName}>Today in {weatherRedux.location?.name}</p>
-                    <p>{weatherRedux.forecast?.forcastday}</p>
-                </div>
-                <div className={styles.weatherIcon}>
-                    <div className={styles.imgIconWraper}>
-                        <img className={styles.imgIcon} src={weatherRedux.current?.condition.icon} alt='weatherIcon'></img>
-                        <p className={styles.temperatureTitle}>{weatherRedux.current?.temp_c}°C</p>
+            <div className={styles.generalPageWrapper}>
+                <div className={styles.infoWrapper}>
+                    <div className={styles.dataWrapper}>
+                        <p className={styles.dayTitle}>{formatDate(weatherRedux.current?.last_updated)}</p>
+                        <p className={styles.cityName}>Today in {weatherRedux.location?.name}</p>
+                        <p>{weatherRedux.forecast?.forcastday}</p>
                     </div>
-                    <p className={styles.feelsLikeTitle}>odczuwalna {weatherRedux.current?.feelslike_c}°C</p>
+                    <div className={styles.weatherIcon}>
+                        <div className={styles.imgIconWraper}>
+                            <img className={styles.imgIcon} src={weatherRedux.current?.condition.icon} alt='weatherIcon'></img>
+                            <p className={styles.temperatureTitle}>{weatherRedux.current?.temp_c}°C</p>
+                        </div>
+                        <p className={styles.feelsLikeTitle}>odczuwalna {weatherRedux.current?.feelslike_c}°C</p>
+                    </div>
+                </div>
+                <div className={styles.nextWeatherDayWraper}>
+                    {forecastdayList ? 
+                        forecastdayList.forecastday.slice(1).map(day => (
+                        <div key={(Math.floor(Math.random() * 1000))}>
+                            <div className={styles.weatherTemeratureNext}>
+                                <p className={styles.dateTitle}>{formatDate(day.date)}</p>
+                                <p className={styles.temp}>{day.day.maxtemp_c}°C</p>
+                            </div>
+                            <img alt="weatherIcon" src={day.day.condition.icon}></img>
+                        </div>
+                    )) 
+                    : 
+                    null }
                 </div>
             </div>
             }
-            <div>
-                {forecastdayList ? forecastdayList.forecastday.slice(1).map(day => (
-                    <p>{day.day.maxtemp_c}</p>
-                )) : null }
-            </div>
         </div>
     )
 }
